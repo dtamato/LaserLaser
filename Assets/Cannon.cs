7e3199136f983fa474e2f341a0a1bf714b359e57
Dave Tamayo <djbtamayo@gmail.com>
2016-10-24 22:09:32 -0400

    Initial push of Unity project

---
+++ Assets/Cannon.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[DisallowMultipleComponent]
+public class Cannon : MonoBehaviour {
+
+	[SerializeField] float rotationSpeed;
+	[SerializeField] float maxBlastForce;
+
+	Player storedPlayer;
+
+	void Update () {
+
+		if (storedPlayer) {
+
+			if (Input.GetAxis ("Horizontal") < 0) {
+				//Debug.Log ("Horizontal: " + Input.GetAxis ("Horizontal"));
+				this.transform.Rotate(rotationSpeed * Vector3.forward);
+			}
+			else if(Input.GetAxis ("Horizontal") > 0) {
+				//Debug.Log ("Horizontal: " + Input.GetAxis ("Horizontal"));
+				this.transform.Rotate(-rotationSpeed * Vector3.forward);
+			}
+
+
+			if (Input.GetButtonDown ("Jump")) {
+				//Debug.Log ("Fire!");
+				StartCoroutine (TempDisableCollider ());
+				storedPlayer.GetComponent<Rigidbody2D>().isKinematic = false;
+				storedPlayer.GetComponent<Rigidbody2D>().AddForce (maxBlastForce * this.transform.up);
+			}
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+
+		if (other.CompareTag ("Player")) {
+
+			storedPlayer = other.GetComponentInChildren<Player> ();
+			this.GetComponentInChildren<SpriteRenderer> ().color = Color.green;
+		}
+	}
+
+	IEnumerator TempDisableCollider () {
+
+		this.GetComponent<Collider2D> ().enabled = false;
+
+		yield return new WaitForSeconds (0.5f);
+
+		this.GetComponent<Collider2D> ().enabled = true;
+	}
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[DisallowMultipleComponent]
+[RequireComponent(typeof(Collider2D), typeof(Rigidbody2D))]
+public class Player : MonoBehaviour {
+
+	Rigidbody2D rb2d;
+
+	void Awake () {
+
+		rb2d = this.GetComponentInChildren<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+
+		if (other.CompareTag ("Cannon")) {
+
+			rb2d.isKinematic = true;
+			this.transform.position = other.transform.position;
+		}
+	}
+}
