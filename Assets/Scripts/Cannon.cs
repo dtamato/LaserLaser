4a79495238fd363f9bf1a34e5ea72af5730f7d7a
Dave Tamayo <djbtamayo@gmail.com>
2016-11-10 00:23:06 -0500

    Imported Rewired plugin Got four player in Put in music and sound effects

---
+++ Assets/Scripts/Cannon.cs
@@ -0,0 +1,80 @@
+﻿using Rewired;
+using UnityEngine;
+using System.Collections;
+
+[DisallowMultipleComponent]
+public class Cannon : MonoBehaviour {
+
+	[SerializeField] int playerId;
+	[SerializeField] float rotationSpeed;
+	[SerializeField] float maxBlastForce;
+
+	Player rewiredPlayer;
+	Laser storedLaser;
+
+	void Awake () {
+
+		rewiredPlayer = ReInput.players.GetPlayer (playerId);
+	}
+
+	void Update () {
+
+		if (storedLaser) {
+
+			if (rewiredPlayer.GetAxis ("Horizontal") < 0) {
+				//Debug.Log ("Horizontal: " + Input.GetAxis ("Horizontal"));
+				this.transform.Rotate(rotationSpeed * Vector3.forward);
+			}
+			else if(rewiredPlayer.GetAxis ("Horizontal") > 0) {
+				//Debug.Log ("Horizontal: " + Input.GetAxis ("Horizontal"));
+				this.transform.Rotate(-rotationSpeed * Vector3.forward);
+			}
+
+
+			if (rewiredPlayer.GetButtonDown ("Fire")) {
+				//Debug.Log ("Fire!");
+				StartCoroutine (TempDisableCollider ());
+				ShootOutPlayer(storedLaser.GetComponentInChildren<Rigidbody2D>());
+				this.GetComponent<AudioSource> ().pitch = Random.Range (0.5f, 1.5f);
+				this.GetComponent<AudioSource> ().Play ();
+			}
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+
+		if (!storedLaser && other.CompareTag ("Player")) {
+
+			storedLaser = other.GetComponentInChildren<Laser> ();
+		}
+	}
+
+	IEnumerator TempDisableCollider () {
+
+		this.GetComponent<Collider2D> ().enabled = false;
+
+		yield return new WaitForSeconds (0.25f);
+
+		this.GetComponent<Collider2D> ().enabled = true;
+
+		ResetCannon();
+	}
+
+	void ShootOutPlayer (Rigidbody2D playerRigidbody) {
+
+		playerRigidbody.isKinematic = false;
+		playerRigidbody.AddForce (maxBlastForce * this.transform.up);
+		storedLaser.transform.GetComponent<SpriteRenderer>().enabled = true;
+		storedLaser = null;
+	}
+
+	void ResetCannon () {
+
+
+	}
+
+	public int GetPlayerID () {
+
+		return playerId;
+	}
+}
---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Item : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+		Destroy(this.gameObject, 10);
+	}
+
+	void Update () {
+
+		Color currentColor = this.GetComponentInChildren<SpriteRenderer>().color;
+		this.GetComponentInChildren<SpriteRenderer>().color = currentColor - new Color(0, 0, 0, 0.1f * Time.deltaTime);
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+
+		if (other.transform.name.Contains("Laser")) {
+
+			this.GetComponent<Collider2D> ().enabled = false;
+			this.GetComponent<SpriteRenderer> ().enabled = false;
+			this.GetComponent<AudioSource> ().Play ();
+			AudioClip clip = this.GetComponent<AudioSource> ().clip;
+			Destroy(this.gameObject, clip.length);
+		}
+	}
+}
