77fcc1e6076ce80c2e898168e6374eef62f57770
Andrew MacMillan <andrewmacmilan1@hotmail.com>
2016-11-18 02:58:35 -0500

    Fixed Game loop, added combo counter and UI. Player's score got messed up (needs reformatting)

--- Assets/Scripts/Cannon.cs
+++ Assets/Scripts/Cannon.cs
@@ -1,6 +1,8 @@
 ﻿using Rewired;
 using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 [DisallowMultipleComponent]
 public class Cannon : MonoBehaviour {
@@ -10,85 +12,147 @@ public class Cannon : MonoBehaviour {
 	[SerializeField] float maxBlastForce = 2200;
 	[SerializeField] float maxAngleOffset = 70;
 
+    //External References.
 	Player rewiredPlayer;
 	Laser storedLaser;
-	float currentAngle;
+    GameManager gameManager;
+
+    //Variables handling player rotation.
+    float currentAngle;
 	float angleOffset;
 	float baseAngle;
 	float minAngle;
 	float maxAngle;
 	int rotationModifier = 1;
 
-	void Awake () {
+    //Lobby Handling Variables.
+    bool inLobby = false;
+    bool playerIsActive = false;
+    GameObject joinUI = null;
+    Text scoreUI = null;
+    public bool gameOver = false;
 
-		rewiredPlayer = ReInput.players.GetPlayer (playerId);
+
+	void Awake ()
+    {
+        gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+        rewiredPlayer = ReInput.players.GetPlayer (playerId);
 		if (maxAngleOffset < 0) { maxAngleOffset *= -1; }
 		SetNewBaseAngle ();
-	}
 
-	void Update () {
+        //If Lobby is the active scene.
+        if (SceneManager.GetActiveScene().buildIndex == 0) {
+            inLobby = true;
+            joinUI = GameObject.Find("PlayerJoin" + playerId);
+        }
+        //If Main Game is the active scene.
+        else if (SceneManager.GetActiveScene().buildIndex == 1) {
+            playerIsActive = gameManager.getPlayerState(playerId);
+            //If the player isn't active, remove their score UI and the player.
+            if (!playerIsActive) {
+                scoreUI = GameObject.Find("PlayerScore" + playerId).GetComponent<Text>();
+                scoreUI.text = "          ";
+                gameObject.SetActive(false);
+            }
+        }
+    }
 
-		if (storedLaser) {
-
+	void Update ()
+    {
+        //Enable Input from Player.
+		if (storedLaser)
 			ProcessInputs ();
-		}
 	}
 
-	void OnTriggerEnter2D(Collider2D other) {
-
-		if (!storedLaser && other.CompareTag ("Player")) {
-
-			storedLaser = other.GetComponentInChildren<Laser> ();
-		}
+	void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!storedLaser && other.CompareTag ("Player"))
+            storedLaser = other.GetComponentInChildren<Laser> ();
 	}
 
-	void ProcessInputs () {
+	void ProcessInputs ()
+    {
+        if (!playerIsActive && inLobby) {
+            if (rewiredPlayer.GetButtonDown("Fire")) {
+                playerIsActive = true;
+                gameManager.setPlayerState(playerId);
+                joinUI.SetActive(false);
+            }
+        }
 
-		// Change direction of rotation if upside down
-		if (this.transform.up == Vector3.up) {
+        //Once game over is triggered take away control from the player.
+        //Only allow start to be pressed to return to lobby.
+        if (gameOver && !inLobby) {
+            playerIsActive = false;
+            if (rewiredPlayer.GetButtonDown("StartGame"))
+            {
+                SceneManager.LoadScene(0);
+            }
+        }
 
-			rotationModifier = 1;
-		}
-		else if (this.transform.up == Vector3.down) {
 
-			rotationModifier = -1;
-		}
+        if (playerIsActive)
+        {
+            // Change direction of rotation if upside down
+            if (this.transform.up == Vector3.up)
+                rotationModifier = 1;
+            else if (this.transform.up == Vector3.down)
+                rotationModifier = -1;
 
-		// Get controller joystick input
-		if (rewiredPlayer.GetAxis ("Horizontal") < 0) {
-			//Debug.Log ("Horizontal: " + Input.GetAxis ("Horizontal"));
-			this.transform.Rotate(rotationSpeed * rotationModifier * Vector3.forward);
-		}
-		else if(rewiredPlayer.GetAxis ("Horizontal") > 0) {
-			//Debug.Log ("Horizontal: " + Input.GetAxis ("Horizontal"));
-			this.transform.Rotate(-rotationSpeed * rotationModifier * Vector3.forward);
-		}
+            // Get controller joystick input
+            if (rewiredPlayer.GetAxis("Horizontal") < 0)
+            {
+                //Debug.Log ("Horizontal: " + Input.GetAxis ("Horizontal"));
+                this.transform.Rotate(rotationSpeed * rotationModifier * Vector3.forward);
+            }
+            else if (rewiredPlayer.GetAxis("Horizontal") > 0)
+            {
+                //Debug.Log ("Horizontal: " + Input.GetAxis ("Horizontal"));
+                this.transform.Rotate(-rotationSpeed * rotationModifier * Vector3.forward);
+            }
 
-		// Restrict angle
-		currentAngle = this.transform.rotation.eulerAngles.z;
-		if(currentAngle < 0) { currentAngle += 360; }
+            // Restrict angle
+            currentAngle = this.transform.rotation.eulerAngles.z;
+            if (currentAngle < 0) { currentAngle += 360; }
 
-		angleOffset = Mathf.Abs(currentAngle - baseAngle);
+            angleOffset = Mathf.Abs(currentAngle - baseAngle);
 
-		//Debug.Log ("zAngle: " + zAngle);
+            //Debug.Log ("zAngle: " + zAngle);
 
-		if (currentAngle >= maxAngle && currentAngle <= maxAngle + 5) {
+            if (currentAngle >= maxAngle && currentAngle <= maxAngle + 5)
+            {
 
-			this.transform.rotation = Quaternion.Euler (0, 0, maxAngle);
-		}
-		else if (currentAngle <= minAngle && currentAngle >= minAngle - 5) {
+                this.transform.rotation = Quaternion.Euler(0, 0, maxAngle);
+            }
+            else if (currentAngle <= minAngle && currentAngle >= minAngle - 5)
+            {
 
-			this.transform.rotation = Quaternion.Euler (0, 0, minAngle);
-		}
+                this.transform.rotation = Quaternion.Euler(0, 0, minAngle);
+            }
 
-		if (rewiredPlayer.GetButtonDown ("Fire")) {
-			//Debug.Log ("Fire!");
-			StartCoroutine (TempDisableCollider ());
-			ShootOutPlayer(storedLaser.GetComponentInChildren<Rigidbody2D>());
-			this.GetComponent<AudioSource> ().pitch = Random.Range (0.5f, 1.5f);
-			this.GetComponent<AudioSource> ().Play ();
-		}
-	}
+            if (rewiredPlayer.GetButtonDown("Fire"))
+            {
+                //Debug.Log ("Fire!");
+                StartCoroutine(TempDisableCollider());
+                ShootOutPlayer(storedLaser.GetComponentInChildren<Rigidbody2D>());
+                this.GetComponent<AudioSource>().pitch = Random.Range(0.5f, 1.5f);
+                this.GetComponent<AudioSource>().Play();
+            }
+
+            //If the player presses B while in the lobby, the player is removed from play.
+            if (rewiredPlayer.GetButtonDown("Back") && inLobby)
+            {
+                playerIsActive = false;
+                gameManager.setPlayerState(playerId);
+                joinUI.SetActive(true);
+            }
+
+            //If the player 1 presses Start while in the lobby, the game begins with whatever players are joined.
+            if (rewiredPlayer.GetButtonDown("StartGame") && inLobby && playerId == 0) {
+                SceneManager.LoadScene(1);
+            }
+        }
+    }
 
 	IEnumerator TempDisableCollider () {
 
--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -1,65 +1,78 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 [DisallowMultipleComponent]
 [RequireComponent(typeof(Collider2D), typeof(Rigidbody2D))]
-public class Laser : MonoBehaviour {
+public class Laser : MonoBehaviour
+{
 
-	[SerializeField] GameObject cannon;
-	[SerializeField] Text scoreText;
+    [SerializeField] GameObject cannon;
+    [SerializeField] Text scoreText;
     [SerializeField] private Text comboText;
 
-	Rigidbody2D rb2d;
-	int score = 0;
+    Rigidbody2D rb2d;
+
+    public int score = 0;
+    public int comboCount = 0;
     private int crystalCount = 0;
-    private int comboCount = 0;
-	void Awake () {
 
-		rb2d = this.GetComponentInChildren<Rigidbody2D> ();
-		this.GetComponentInChildren<TrailRenderer> ().sortingLayerName = this.GetComponent<SpriteRenderer> ().sortingLayerName;
-		this.GetComponentInChildren<TrailRenderer> ().sortingOrder = this.GetComponent<SpriteRenderer> ().sortingOrder - 1;
-	}
+    void Awake()
+    {
 
-	void OnCollisionEnter2D(Collision2D other) {
+        rb2d = this.GetComponentInChildren<Rigidbody2D>();
+        this.GetComponentInChildren<TrailRenderer>().sortingLayerName = this.GetComponent<SpriteRenderer>().sortingLayerName;
+        this.GetComponentInChildren<TrailRenderer>().sortingOrder = this.GetComponent<SpriteRenderer>().sortingOrder - 1;
+    }
 
-		if(other.transform.CompareTag("Boundary")) {
+    void OnCollisionEnter2D(Collision2D other)
+    {
 
-			rb2d.isKinematic = true;
-			cannon.transform.position = other.contacts[0].point;
-			cannon.transform.rotation = other.transform.rotation;
-			this.transform.position = cannon.transform.position + 1.5f * cannon.transform.up;
-			this.transform.GetComponent<SpriteRenderer>().enabled = false;
-			cannon.GetComponentInChildren<Cannon> ().SetNewBaseAngle ();
+        if (other.transform.CompareTag("Boundary"))
+        {
 
-		    if (crystalCount == 0)
-		    {
+            rb2d.isKinematic = true;
+            cannon.transform.position = other.contacts[0].point;
+            cannon.transform.rotation = other.transform.rotation;
+            this.transform.position = cannon.transform.position + 1.5f * cannon.transform.up;
+            this.transform.GetComponent<SpriteRenderer>().enabled = false;
+            cannon.GetComponentInChildren<Cannon>().SetNewBaseAngle();
+
+            if (crystalCount == 0)
+            {
                 comboCount = 0;
             }
-		    else
-		    {
-		        comboCount++;
-		        crystalCount = 0;
-		    }
-		        
-		}
-	}
+            else
+            {
+                comboCount++;
+                crystalCount = 0;
+            }
 
-	void OnTriggerEnter2D(Collider2D other) {
+        }
+    }
 
-		//Destroy(this.gameObject);
-		if(other.CompareTag("Item"))
-		{
-		    crystalCount++;
-		    //Debug.Log("I have a combo of: " + comboCount);
-			score++;
-			scoreText.text = "P" + (cannon.GetComponent<Cannon> ().GetPlayerID () + 1) + "- " + score.ToString("00");
-		    
-		}
-	}
+    void OnTriggerEnter2D(Collider2D other)
+    {
+
+        //Destroy(this.gameObject);
+        if (other.CompareTag("Item"))
+        {
+            crystalCount++;
+            //Debug.Log("I have a combo of: " + comboCount);
+            score++;
+            scoreText.text = "P" + (cannon.GetComponent<Cannon>().GetPlayerID() + 1) + "- " + score.ToString("00");
+
+        }
+    }
 
     void Update()
     {
-        comboText.text = "Combo: " + comboCount;
+        if (Application.loadedLevelName == "Main")
+        {
+            comboText.text = "Combo: " + comboCount;
+            //Debug.Log(comboCount);
+        }
+            
     }
 }
