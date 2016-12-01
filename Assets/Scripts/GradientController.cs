50e260293ee26718b876bb22e66a2ee3a79a91d4
Andrew MacMillan <andrewmacmillan1@hotmail.com>
2016-12-01 15:02:51 -0500

    Aded new cene to work in, created shader for gradients

---
+++ Assets/Scripts/GradientController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GradientController : MonoBehaviour {
+
+    [SerializeField] private Color colourTop = Color.blue;
+    public Color colourBottom = Color.black;
+    public Color newColour;
+
+
+    public Material yourGradientMaterial;
+
+    void Start()
+    {
+        
+        yourGradientMaterial.SetColor("_Color2", Color.black); // the bottom color
+    }
+
+    void Update()
+    {
+        yourGradientMaterial.SetColor("_Color", colourTop); // the top color
+    }
+}
