bb709d4c927e2f4ee20cbe5322098ab38a782f51
Dave Tamayo <djbtamayo@gmail.com>
2017-01-28 00:22:31 -0500

    - Added new fonts and the background grid sprite - Started working on the functionality for the title screen

---
+++ Assets/Scripts/TitleSceneBar.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[DisallowMultipleComponent]
+public class TitleSceneBar : MonoBehaviour {
+
+	[Header("Parameters")]
+	[SerializeField] float startDelay = 0;
+	[SerializeField] float moveSpeed = 1;
+
+	[Header("References")]
+	[SerializeField] GameObject colorBar;
+	[SerializeField] Transform startPointTransform;
+	[SerializeField] Transform endPointTransform;
+
+
+	void Start () {
+
+		moveSpeed = Random.Range(moveSpeed * 0.5f, moveSpeed * 1.5f);
+		colorBar.transform.localPosition = startPointTransform.localPosition;
+	}
+
+	void Update () {
+
+		if(Time.time > startDelay && colorBar.transform.localPosition != endPointTransform.localPosition) {
+
+			colorBar.transform.localPosition = Vector3.Lerp(colorBar.transform.localPosition, endPointTransform.localPosition, moveSpeed * Time.deltaTime);
+		}
+	}
+}
