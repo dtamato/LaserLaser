606c76017d33805e121345a53a8fe267ec3f5646
Dave Tamayo <djbtamayo@gmail.com>
2017-02-03 18:25:48 -0500

    - Fixed the border color-changing for FFA. Implemented it for TB - Reduced the bounciness on the Bouncy material - Changed color of diamond since one of the player colors is yellow now - Connected scenes in preparation for Friday's playtesting session - Made player default colors in Lobby to Thi's

--- Assets/Scripts/LobbyScripts/LobbyManager.cs
+++ Assets/Scripts/LobbyScripts/LobbyManager.cs
@@ -16,22 +16,21 @@ public class ColorList
     }
 }
 
-
 public class LobbyManager : MonoBehaviour {
 
 
     void FFAColourList() //The available colours for the FFA lobby
     {
-        _colorlist[0] = new ColorList(false, Color.red);
-        _colorlist[1] = new ColorList(false, Color.blue);
-        _colorlist[2] = new ColorList(false, Color.green);
-        _colorlist[3] = new ColorList(false, Color.magenta);
-        _colorlist[4] = new ColorList(true, Color.yellow); //need to change due to it being too close to the crystal's colour
+		_colorlist[0] = new ColorList(false, new Color(1, 0, 0.4f)); // Thi's Magenta
+		_colorlist[1] = new ColorList(false, new Color(0.21f, 1, 0.13f)); // Thi's Green
+		_colorlist[2] = new ColorList(false, new Color(1, 0.89f, 0)); // Thi's Yellow
+		_colorlist[3] = new ColorList(false, new Color(0.31f, 1, 0.87f)); // Thi's Blue
+        _colorlist[4] = new ColorList(true, Color.magenta);
         _colorlist[5] = new ColorList(true, Color.cyan);
-        _colorlist[6] = new ColorList(true, new Color(0.29f, 0.35f, 0.67f, 1f));
-        _colorlist[7] = new ColorList(true, new Color(0.95f, 0.62f, 0f, 1f));
-        _colorlist[8] = new ColorList(true, new Color(0f, 0.95f, 0.7f, 1f));
-        _colorlist[9] = new ColorList(true, new Color(0.65f, 0f, 0.73f, 1f));
+        _colorlist[6] = new ColorList(true, new Color(0.29f, 0.35f, 0.67f, 1f)); // Deep violet
+        _colorlist[7] = new ColorList(true, new Color(0.95f, 0.62f, 0f, 1f)); // Orange
+        _colorlist[8] = new ColorList(true, new Color(0f, 0.95f, 0.7f, 1f)); // Teal
+        _colorlist[9] = new ColorList(true, new Color(0.65f, 0f, 0.73f, 1f)); // Purple
     }
 
     private BaseGM gameManager;
