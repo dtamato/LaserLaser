08bf3358bbd173fabd8ec23d05e59f711f16be02
Carolin Hanna <carolinhanna@hotmail.com>
2017-02-09 13:45:57 -0500

    BigBall Power Up Created. Tested & works. Created a prefab for the powerup. Added the power up to the powerup spawner.

---
+++ Assets/Scripts/PowerUps/BigBall.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BigBall : MonoBehaviour 
+{
+	[SerializeField] float bigTimer = 4;
+	[SerializeField] float size = 1;
+	private GameObject temp;
+	private GameObject[] players = new GameObject[4];
+
+	// Use this for initialization
+	void Start () 
+	{
+		players = GameObject.FindGameObjectWithTag ("GameManager").GetComponent<BaseGM> ().GetActivePlayers ();
+		
+	}
+	
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		// Make BigBall Icon disappear.
+		gameObject.GetComponent<SpriteRenderer> ().enabled = false;
+		gameObject.GetComponent<Collider2D> ().enabled = false;
+
+		// Make the player that collided to the BigBall, have a bigger ball.
+		temp = other.gameObject;
+		other.transform.localScale += new Vector3 (size, size, size);
+		StartCoroutine (DisablePowerUp ());
+	}
+
+	IEnumerator DisablePowerUp ()
+	{
+		yield return new WaitForSeconds (bigTimer);
+		temp.transform.localScale = Vector3.one;
+		Destroy (this.gameObject);
+	}
+}
