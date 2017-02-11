27df2a0e56ace38ff7b34ddd3557e7cab7aad7b6
Rebecca <rebeccamitchell88@gmail.com>
2017-02-10 19:56:05 -0500

    PARTICLE CITY

---
+++ Assets/Particle Tests/CircleParticle.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleParticle : MonoBehaviour {
+
+    ParticleSystem ps;
+
+	// Use this for initialization
+	void Start () {
+        ps = GetComponent<ParticleSystem>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float increase = 20 * Time.deltaTime;
+        this.transform.localScale += new Vector3(increase, increase, increase);
+        var mainPS = ps.main;
+        Color reduceColor = new Color(0, 0, 0, 0.5f * Time.deltaTime);
+        Color psColor = mainPS.startColor.color;
+        Color newColor = psColor - reduceColor;
+        mainPS.startColor = newColor;
+
+        if(this.transform.localScale.x > 300)
+        {
+            Destroy(this.gameObject);
+        }
+	}
+
+    public void ChangeColor (Color newColor)
+    {
+        ps = GetComponent<ParticleSystem>();
+        var mainPS = ps.main;
+        mainPS.startColor = newColor;
+    }
+}
