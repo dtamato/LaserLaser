52d7db69c30f781030f5c31436c802437a46fe8f
Carolin Hanna <carolinhanna@hotmail.com>
2017-03-23 14:53:23 -0400

    Created the AreaBlast powerup. NOT DONE. Still needs fixing.

---
+++ Assets/Scripts/PowerUps/AreaBlast/AreaBlast.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaBlast : MonoBehaviour 
+{
+	// Timer for the icon.
+	[SerializeField] float areaTimer = 5;
+	[SerializeField] GameObject areaBlaster;
+
+	void Start()
+	{
+		StartCoroutine (DisablePowerUp ());
+	}
+
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		// Make the Area Blast Icon Disappear.
+		gameObject.GetComponent<SpriteRenderer> ().enabled = false;
+		gameObject.GetComponent<Collider2D> ().enabled = false;
+		// Extends the destroy timer.
+		areaTimer += 5;
+
+		// Instantiate a Circle with area of effect.
+		GameObject newAreaBlaster = Instantiate(areaBlaster, other.transform.position, other.transform.rotation) as GameObject;
+		// Change the color of the circle to the players color.
+		newAreaBlaster.GetComponent<SpriteRenderer> ().color = other.gameObject.GetComponent<SpriteRenderer> ().color;
+		// Set the player id & team  to the player that collided with this powerup.
+		newAreaBlaster.GetComponent<Blaster> ().playerNum = other.gameObject.GetComponent<Laser> ().myPlayerID;
+		newAreaBlaster.GetComponent<Blaster> ().teamNum = other.gameObject.GetComponent<Laser> ().myTeam;
+	}
+
+	// Disable the power up after set time.
+	IEnumerator DisablePowerUp()
+	{
+		yield return new WaitForSeconds (areaTimer);
+		Destroy (this.gameObject);
+	}
+}
---
+++ Assets/Scripts/PowerUps/AreaBlast/Blaster.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blaster : MonoBehaviour 
+{
+	// Timer for the blaster.
+	[SerializeField] float blasterTimer = 5;
+
+	BaseGM gameManager;
+	public int playerNum;
+	public int teamNum;
+	public int score;
+	public string gameMode;
+	private float transparent = 0.5f;
+
+	void Awake()
+	{
+		gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<BaseGM>();
+		gameMode = gameManager.gameMode;
+
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.CompareTag("Diamonds"))
+		{
+			newScore();
+		}
+	}
+
+	public void newScore()
+	{
+		// FFA scoring.
+		if (gameMode == "FFA")
+		{
+			score++;
+			gameManager.addScore(playerNum, score);
+		}
+		// Team scoring.
+		else
+		{
+			gameManager.addToTeamScore (teamNum == 1);
+		}
+	}
+
+	IEnumerator DistroyBlaster()
+	{
+		yield return new WaitForSeconds (blasterTimer);
+		Destroy (this.gameObject);
+	}
+}
